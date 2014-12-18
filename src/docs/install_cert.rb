4f525e5a5819a5d91d222210afa69d3c1f914184
rinate <rinat.eilon@gmail.com>
2014-12-18 12:16:24 -0500

    fix installation instructions

--- src/docs/install_cert.rb
+++ src/docs/install_cert.rb
@@ -1,16 +1,15 @@
 require 'net/http'
  	
-# create a path to the file "C:\RailsInstaller\cacert.pem"
+# create a path to the file "C:\temp\cacert.pem"
 cacert_file = File.join(%w{C: temp cacert.pem})
  
 Net::HTTP.start("curl.haxx.se") do |http|
   resp = http.get("/ca/cacert.pem")
   if resp.code == "200"
     open(cacert_file, "wb") { |file| file.write(resp.body) }
-    puts "\n\nA bundle of certificate authorities has been installed to"
-    puts "C:\\RailsInstaller\\cacert.pem\n"
-    puts "* Please set SSL_CERT_FILE in your current command prompt session with:"
-    puts "     set SSL_CERT_FILE=C:\\RailsInstaller\\cacert.pem"
+    puts "\n\nTo complete this step (assuming your ruby installation is in <RUBY_DIRECTORY> ) :"
+    puts "- copy c:\\temp\\cacert.pem file to <RUBY_DIRECTORY>\\cacert.pem\n"
+    puts "- set SSL_CERT_FILE to <RUBY_DIRECTORY>\\cacert.pem"
     puts "* To make this a permanent setting, add it to Environment Variables"
     puts "  under Control Panel -> Advanced -> Environment Variables"
   else
